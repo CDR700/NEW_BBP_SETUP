1dbb8b5571fa3d43dfb49c25574eebba853b5722
cdr700 <radiation8@gmail.com>
2024-11-10 16:35:23 +0100

    Update newbbp_recon_scripts_generator.sh

--- newbbp_recon_scripts_generator.sh
+++ newbbp_recon_scripts_generator.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
 this_dir=$(pwd)
-
-ua_h=$(<"$this_dir/ua.txt")
-
+ua_h=cat "$this_dir/ua.txt"
 echo "Building recon scripts and various one-liners"
-sleep 1
+sleep 1; 
 
 touch "$this_dir/subfinder.wildcards.sh"
 chmod +x "$this_dir/subfinder.wildcards.sh"
 cat <<EOL > "$this_dir/subfinder.wildcards.sh"
 #!/bin/bash
 
-# Subfinder and ffuf script to look for subdomains
+#subfinder and ffuf script to look for subdomains
 
-subfinder TODO
+subfinder 
 EOL
+
 
 touch "$this_dir/ffuf-subdomain.wildcards.sh"
 chmod +x "$this_dir/ffuf-subdomain.wildcards.sh"
-cat <<EOL > "$this_dir/ffuf-subdomain.wildcards.sh"
+cat <<'EOL' > "$this_dir/ffuf-subdomain.wildcards.sh"
 #!/bin/bash
 for URL in \$(<scopes.wildcards.domains.txt); do
    ffuf -mc 200 -w ~/wordlists/subdomains_all.txt -u FUZZ.\$URL -ac -H "$ua_h"
