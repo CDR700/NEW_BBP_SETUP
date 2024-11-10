9ce9918836d02b8ea3ab3ec1f74486eecfd3fa1a
cdr700 <radiation8@gmail.com>
2024-11-10 16:19:44 +0100

    Create newbbp_recon_scripts_generator.sh

---
+++ newbbp_recon_scripts_generator.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+this_dir=$(pwd)
+ua_h=cat "$this_dir/ua.txt"
+echo "Building recon scripts and various one-liners"
+
+touch "$this_dir/subfinder.wildcards.sh"
+chmod +x "$this_dir/subfinder.wildcards.sh"
+cat <<EOL > "$this_dir/subfinder.wildcards.sh"
+#!/bin/bash
+
+#subfinder and ffuf script to look for subdomains
+
+subfinder 
+EOL
+
+touch "$this_dir/ffuf-subdomain.wildcards.sh"
+chmod +x "$this_dir/ffuf-subdomain.wildcards.sh"
+cat <<EOL > "$this_dir/ffuf-subdomain.wildcards.sh"
+#!/bin/bash
+for URL in $(<scopes.wildcards.domains.txt); do
+   ffuf -mc 200 -w ~/wordlists/subdomains_all.txt -u FUZZ."$URL" -ac -H "$ua_h"
+done
+EOL
+
+clear
+echo "'$this_dir' recon scripts ready, happy hunting!"
+sleep 1;
