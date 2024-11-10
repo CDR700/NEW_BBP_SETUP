c84ac2fb9cc07dc1e523436dd5a60be03a843596
cdr700 <radiation8@gmail.com>
2024-11-10 16:41:31 +0100

    Update newbbp_recon_scripts_generator.sh

--- newbbp_recon_scripts_generator.sh
+++ newbbp_recon_scripts_generator.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 this_dir=$(pwd)
-ua_h=cat($this_dir/ua.txt)
+ua_h=$(<"$this_dir/ua.txt")
 
 echo "Building recon scripts and various one-liners"
 sleep 1; 
+
+touch "$this_dir/ffuf-subdomain.wildcards.sh"
+chmod +x "$this_dir/ffuf-subdomain.wildcards.sh"
+cat <<'EOL' > "$this_dir/ffuf-subdomain.wildcards.sh"
+#!/bin/bash
+for URL in \$(<scopes.wildcards.domains.txt); do
+   ffuf -mc 200 -w ~/wordlists/subdomains_all.txt -u FUZZ.\$URL -ac -H "$ua_h"
+done
+EOL
+
 
 touch "$this_dir/subfinder.wildcards.sh"
 chmod +x "$this_dir/subfinder.wildcards.sh"
@@ -15,16 +25,5 @@ cat <<EOL > "$this_dir/subfinder.wildcards.sh"
 subfinder 
 EOL
 
-
-touch "$this_dir/ffuf-subdomain.wildcards.sh"
-chmod +x "$this_dir/ffuf-subdomain.wildcards.sh"
-cat <<'EOL' > "$this_dir/ffuf-subdomain.wildcards.sh"
-#!/bin/bash
-for URL in \$(<scopes.wildcards.domains.txt); do
-   ffuf -mc 200 -w ~/wordlists/subdomains_all.txt -u FUZZ.\$URL -ac -H "$ua_h"
-done
-EOL
-
-clear
 echo "'$this_dir' recon scripts ready, happy hunting!"
 sleep 1
